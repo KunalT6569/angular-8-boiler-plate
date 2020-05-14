bc47481c964a55eeac1e2e34b6c6cb4ebaf99ee3
Kunal Tandon <kunal.tandon@instasolv.com>
2020-05-14 15:05:31 +0530

    bootstrap template

---
+++ src/app/ngx-bootstrap/ngx-bootstrap.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class NgxBootstrapModule { }
